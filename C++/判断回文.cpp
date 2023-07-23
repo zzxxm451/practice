f0c99b1124d98d007568e67f251eb17a4da6913d
zzxxm451 <135724024+zzxxm451@users.noreply.github.com>
2023-07-23 14:14:47 +0800

    Create 判断回文.cpp

---
+++ C++/判断回文.cpp
@@ -0,0 +1,65 @@
+
+#include<stdio.h>
+#include<string.h>
+
+#include<iostream>
+using namespace std;
+#define Max 50
+
+typedef struct{
+	int top;
+	char a[Max];
+}Sqstack;
+
+void Init(Sqstack &str){
+	str.top=-1;
+	for(int i=0;i<Max;i++){
+		str.a[i]='\0';
+	}
+}
+
+void Pop(Sqstack &str){
+/*	do{
+		str.top++;
+        //printf("%d\n",str.top);
+		scanf("%c",&(str.a[str.top]));printf("%c,%d",(str.a[str.top]),str.top);
+	}while(str.a[str.top]!='\n'); //防止栈溢出
+    str.a[str.top]='\0';*/
+    
+    //str.top--;
+    cin.getline(str.a,Max,'\n');
+    str.top=strlen(str.a)-1;
+    //printf("%s %d\n",str.a,str.top);
+}
+
+void Push(Sqstack &str1,Sqstack &str2){
+	for(int i=0;i<=str1.top;i++){ 
+		str2.a[i]=str1.a[str1.top-i];
+		printf("%c",str1.a[i]);
+	}
+	str2.top=str1.top;
+}
+
+int main(){
+	Sqstack str;
+	Init(str);
+	Pop(str);
+	Sqstack str1;
+	Init(str1);
+	Push(str,str1);
+	int flag=1;
+	for(int i=0;i<str.top;i++){
+		if(str.a[i]==str1.a[i]){
+			continue;
+		}else{
+			flag=0;
+			break;
+		}
+	}
+	if(flag){
+        printf("是回文。");
+    }else{
+        printf("不是回文。");
+    }
+	return 0;
+}
